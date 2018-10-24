cfb357a77199d716bfad6faf8cbcbe50ee0c8f62
kharade-rohan <kharade@eurecom.fr>
2018-10-24 14:57:20 +0200

    First Commit

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python2.7
 import requests
 import sys
-r = requests.get('https://koinex.in/api/ticker')
-r.json()
-print(r)
+import json
+tw_url='https://api.sandbox.transferwise.tech/v1/rates?source=EUR&target=INR'
+headers={'Authorization' : 'Bearer ce21ac17-c0b4-4ec0-879f-85005c5ed119'}
+resp = requests.get(tw_url,headers=headers)
+r=resp.json()
+#print (resp.content)
+print (r)
+print (r['target'])
+#r.json()
+#print(r)
